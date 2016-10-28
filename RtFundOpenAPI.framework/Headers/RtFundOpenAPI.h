e86b8555af974fca7a4e3f7b7a7545cdb04a2f88
HzHenghaokeji <wangw@henghaojijin.com>
2016-10-28 16:55:59 +0800

    添加封装库

---
+++ RtFundOpenAPI.framework/Headers/RtFundOpenAPI.h
@@ -0,0 +1,27 @@
+//
+//  RtFundOpenAPI.h
+//  RtFundOpenAPI
+//
+//  Created by Kevin on 2016/10/18.
+//  Copyright © 2016年 HenghaoKeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RtFundOpenAPI.
+FOUNDATION_EXPORT double RtFundOpenAPIVersionNumber;
+
+//! Project version string for RtFundOpenAPI.
+FOUNDATION_EXPORT const unsigned char RtFundOpenAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RtFundOpenAPI/PublicHeader.h>
+#import "IMJSON.h"
+#import "FXKeychain.h"
+#import "AFNetworkActivityIndicatorManager.h"
+#import "AFNetworking.h"
+#import "IMTradeRequest.h"
+#import "IMAssetRequest.h"
+#import "IMTradeRecordRequest.h"
+#import "IMHelpTool.h"
+#import "IMHttp"
+
